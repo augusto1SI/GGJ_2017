9d176ceca41d8f5ac127fcd0b0de730618775c4b
Cubonator <rd.ibarrac.859@gmail.com>
2017-01-21 23:23:48 -0600

    Particle Controller attached to Wave

--- Assets/Scripts/WaveCreation/WaveElement.cs
+++ Assets/Scripts/WaveCreation/WaveElement.cs
@@ -6,10 +6,12 @@ public class WaveElement : MonoBehaviour
     public int m_WaveTypeID;
     public Animation m_WaveAnimation;
     public WaveTriggerController m_WaveTriggerController;
+    public WaveParticleController m_WaveParticleController;
 
     void Start()
     {
         m_WaveTriggerController.Initialize(m_WaveAnimation.clip.length);
+        m_WaveParticleController.Initialize(m_WaveAnimation.clip.length);
     }
 
 	public bool TryDoWave ()
@@ -17,6 +19,7 @@ public class WaveElement : MonoBehaviour
 	    if(m_WaveTriggerController.TryDoWave())
         {
             m_WaveAnimation.Play();
+            m_WaveParticleController.ActivateParticles();
             return true;
         }
 
