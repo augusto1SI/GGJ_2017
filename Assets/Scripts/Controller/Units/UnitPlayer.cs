c1bece213c161e4a964b52e3a5b077db0e6df0d9
augusto1SI <augusto@1simpleidea.mx>
2017-01-21 23:07:15 -0600

    trash removed from player

--- Assets/Scripts/Controller/Units/UnitPlayer.cs
+++ Assets/Scripts/Controller/Units/UnitPlayer.cs
@@ -6,13 +6,8 @@ public class UnitPlayer : Unit
 {
 
 	private CharacterController control;
-	private Transform head;
-	private float headPitch = 0f;
-	public float headPitchMax = 60f;
-	public float jumpSpeed = 5f;
 
 	private float gravity = 0f;
-	private float fakeZeroGravity = -0.25f;
 
 	private Vector3 lookPosition = Vector3.zero;
 
@@ -25,8 +20,6 @@ public class UnitPlayer : Unit
 		agent.updatePosition = agent.updateRotation = false;
 
 		control = GetComponent<CharacterController>();
-
-		head = transform.FindChild("Head");
 
 		if(!control)
 		{
@@ -46,22 +39,12 @@ public class UnitPlayer : Unit
 
 		move.Normalize();
 
-//		if(Input.GetButton("Run"))
-//			move*=m_RunSpeed;
-//		else
-			move*=m_WalkSpeed;
+		move*=m_WalkSpeed;
 
 		move=transform.TransformDirection(move);
 
 		//jUMP AND GRAVITY CODE
-		if(control.isGrounded)
-		{
-			if(Input.GetButtonDown("Jump"))
-				gravity=jumpSpeed;
-			else
-				gravity=fakeZeroGravity;
-		}
-		else
+		if(!control.isGrounded)
 		{
 			gravity += Physics.gravity.y * Time.deltaTime;
 		}
@@ -69,32 +52,6 @@ public class UnitPlayer : Unit
 		move.y = gravity;
 
 		control.Move(move *Time.deltaTime);
-
-		//LOOK CODE
-		if(head)
-		{
-			headPitch -= Input.GetAxis("Mouse Y") * m_TurnSpeed * Time.deltaTime;
-			headPitch = Mathf.Clamp(headPitch, -headPitchMax, headPitchMax);
-			head.rotation = transform.rotation;
-			head.Rotate(headPitch, 0f, 0f);
-		}
-
-		//AIMING
-		RaycastHit hit;
-		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f,0.5f,0f));
-
-		lookPosition = Camera.main.transform.position + (Camera.main.transform.forward * Camera.main.farClipPlane);
-
-		if(Physics.Raycast(ray, out hit))
-		{
-			lookPosition = hit.point;
-		}
-
-		if(weapon)
-		{
-			weapon.transform.LookAt(lookPosition);
-			weapon.Fire = Input.GetButton("Fire1");
-		}
 
 		base.Update();
 	}
